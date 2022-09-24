d4292ab9719af98f2d8ab72799b87b965f08d29c
Abhishek Dewan <abhishekdewan2802@gmail.com>
2022-09-23 22:18:12 -0700

    upgrade AGP to latest beta and also starting to make progress

--- app/src/main/kotlin/com/adewan/mystuff/MainActivity.kt
+++ app/src/main/kotlin/com/adewan/mystuff/MainActivity.kt
@@ -3,13 +3,7 @@ package com.adewan.mystuff
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.adewan.mystuff.ui.navigation.AppNavGraph
 import com.adewan.mystuff.ui.theme.MyStuffTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyStuffTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AppNavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyStuffTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/kotlin/com/adewan/mystuff/ui/navigation/AppNavGraph.kt
@@ -0,0 +1,8 @@
+package com.adewan.mystuff.ui.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppNavGraph() {
+    
+}
