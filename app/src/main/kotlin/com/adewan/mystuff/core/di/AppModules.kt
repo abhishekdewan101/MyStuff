aafb9e4d1cd662608a8d020ac7139f8203923e2c
Abhishek Dewan <abhishekdewan2802@gmail.com>
2022-11-28 21:52:58 -0800

    CleanUp: Cleaning up the old application code that we aren't going to be using anymore

--- app/src/main/kotlin/com/adewan/mystuff/core/di/AppModules.kt
+++ app/src/main/kotlin/com/adewan/mystuff/core/di/AppModules.kt
@@ -3,13 +3,8 @@ package com.adewan.mystuff.core.di
 import android.content.Context
 import com.adewan.mystuff.core.network.NetworkDataSource
 import com.adewan.mystuff.core.repository.TmdbRepository
-import com.adewan.mystuff.ui.gamedetails.GameDetailViewModel
-import com.adewan.mystuff.ui.home.HomeViewModel
-import com.adewan.mystuff.ui.moviedetails.MovieDetailViewModel
-import com.adewan.mystuff.ui.tvshowdetails.TvShowDetailViewModel
 import java.time.Clock
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
@@ -22,23 +17,6 @@ val appModule = module {
 
     single {
         Clock.systemDefaultZone()
-    }
-
-    // ViewModel
-    viewModel {
-        HomeViewModel(get(), get())
-    }
-
-    viewModel {
-        GameDetailViewModel(get())
-    }
-
-    viewModel {
-        MovieDetailViewModel(get())
-    }
-
-    viewModel {
-        TvShowDetailViewModel(get())
     }
 
     // Repositories
